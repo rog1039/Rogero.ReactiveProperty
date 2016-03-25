951c918953550c5115e1b34b5042c1d167e2297c
Paul Rogero <paulrogero@gmail.com>
2016-03-25 14:45:58 -0400

    Made library portable.

---
+++ src/Rogero.ReactiveProperty/Rogero.ReactiveProperty.Tests/DecimalTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rogero.ReactiveProperty.Tests
+{
+    public class DecimalTests
+    {
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void CreateDecimalReactiveProperty()
+        {
+            var property = new ReactiveProperty<decimal>();
+            var result = property.Value;
+        }
+    }
+}
--- src/Rogero.ReactiveProperty/Rogero.ReactiveProperty.Tests/Properties/AssemblyInfo.cs
+++ src/Rogero.ReactiveProperty/Rogero.ReactiveProperty.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rogero.ReactiveProperty")]
+[assembly: AssemblyTitle("Rogero.ReactiveProperty.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rogero.ReactiveProperty")]
+[assembly: AssemblyProduct("Rogero.ReactiveProperty.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d77b9fe-c34f-4b84-a33f-758d8c92f2c0")]
 
 // Version information for an assembly consists of the following four values:
 //
